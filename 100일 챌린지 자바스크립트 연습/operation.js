c099c162e4e6a4f63643494482f0937fe09810f9
Jiin <134131529+Jiin4475@users.noreply.github.com>
2023-07-02 23:16:59 +0900

    basic javascript

---
+++ 100일 챌린지 자바스크립트 연습/operation.js
@@ -0,0 +1,30 @@
+// math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);//4가 10에 두번 들어가면 8 => 10-8 = 2 답은 2
+console.log(10 % 3);//10-9= 1 답은1
+console.log((10 + 3 - 5) * 10) ;
+
+
+// let result = (10 + 3 - 5) * 10;
+// result = 10 * 4;
+// result ++; // result = result +1
+// result --; // result = result -1
+
+// result +=5; // result = result +5
+// result -=5; // result = result -5
+// result /=5; // result = result /5
+// result *=5; // result = result *5
+
+// console.log(result);
+console.log('Max'+' '+'LALALALALAL');
+
+let userName = 'Max';
+console.log(userName.length);// 글자 수
+console.log(userName.toUpperCase());//글자 대문자로
+
+let hobbies = ['sports', 'cooking'];
+console.log(hobbies.length);
