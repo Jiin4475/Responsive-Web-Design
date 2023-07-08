5d5093ce305e5000f36d8f66af0a4eabff91653b
Jiin <yujiin4475@gmail.com>
2023-07-08 20:07:09 +0900

    for in

--- 100일 챌린지 자바스크립트 연습02/exercise-2/loops-in-action/loops-in-action.js
+++ 100일 챌린지 자바스크립트 연습02/exercise-2/loops-in-action/loops-in-action.js
@@ -19,3 +19,49 @@ outputResultElement.style.display = 'block';
 
 
 calculateSumButtonElement.addEventListener('click',calculateSum);
+
+//highlight links 
+//for of 예제
+const highlightLinksButtonElement = document.querySelector('#highlight-links button');
+
+
+function highlightLinks(){
+const anchorElements = document.querySelectorAll('#highlight-links a');
+
+for(const anchorElement of anchorElements){
+    anchorElement.classList.add('highlight');
+}
+}
+
+highlightLinksButtonElement.addEventListener('click',highlightLinks);
+
+//display user data
+
+const dummyUserData = {
+    firstName:'Jiin',
+    lastName:'Yu',
+    age:27
+};
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+// for in 객체의 모든 속성에 대해 무언가를 실행해야 할 때
+function displayUserData(){
+    const outputDataElement = document.getElementById('output-user-data');
+    
+    //innerHtml 은 html태그롸 텍스트 전체임 =''; 모든 항목이 지워지는것
+    outputDataElement.innerHTML = '';
+
+
+    //데이타가 담겨있는 dummyuserData에 각 key(first name,last name,age)
+    for(const key in dummyUserData){
+        const newUserDataListItemElement = document.createElement('li');
+
+        const outputText = key.toUpperCase() + ': '+ dummyUserData[key];
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement)
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click',displayUserData);
+
