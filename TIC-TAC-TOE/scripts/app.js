319ba0da17233fefd191718370c0305506cb0d2f
Jiin <yujiin4475@gmail.com>
2023-07-10 16:09:42 +0900

    save playername and get formdata

--- TIC-TAC-TOE/scripts/app.js
+++ TIC-TAC-TOE/scripts/app.js
@@ -1,9 +1,14 @@
 //overlay 나타나기
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+
+//form access 
+const formElement = document.querySelector('form');
+
 //버튼클릭해서 edit 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+
 //캔슬버튼을 누르면 오버레이 사라짐
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 
@@ -13,3 +18,5 @@ editPlayer2BtnElement.addEventListener('click',openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
 backdropElement.addEventListener('click',closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig);
--- TIC-TAC-TOE/scripts/config.js
+++ TIC-TAC-TOE/scripts/config.js
@@ -9,3 +9,12 @@ function closePlayerConfig(){
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
 }
+//form에 플레이어 저장:양식이 제출되면 실행
+function savePlayerConfig(event){
+//브라우저의 기본 동작을 방해
+ event.preventDefault();//페이지 재로드하지 않음
+ const formData = new FormData(event.target);
+ const enteredPlayerName = formData.get('playername');//직접 사용할 수 있는 내장된 함수 FormData()
+ //event는 이 이벤트를 담당한 HTML 요소를 가리키는 타켓속성이 있음
+ console.log(enteredPlayerName);
+}
