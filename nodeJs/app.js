946e0430d341abd721a5f6b74d451e0fe23edc70
Jiin <yujiin4475@gmail.com>
2023-07-14 22:40:42 +0900

    .

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -16,8 +16,15 @@ app.get('/', function(req, res){
    res.render('index');
 });
 
+
+//공유된 레스토랑의 갯수
 app.get('/restaurants', function(req, res){
-    res.render('restaurants');
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+    
+    res.render('restaurants',{ numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants});
 });
 
 app.get('/recommend', function(req, res){
