abc4a4fb0ccd7c9339d372baa384f52fa8756b50
Jiin <yujiin4475@gmail.com>
2023-07-15 21:41:32 +0900

    .

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-
+const uuid = require('uuid');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -27,12 +27,30 @@ app.get('/restaurants', function (req, res) {
   });
 });
 
+app.get ('/restaurants/:id', function (req, res){
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants){
+    if(restaurant.id === restaurantId){
+      return res.render ('restaurant-detail', { restaurant:restaurant });
+    }
+  }
+  //404경고 페이지 레스토랑을 찾을 수 없을 때
+res.render('404');
+});
+
+
 app.get('/recommend', function (req, res) {
   res.render('recommend');
 });
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();//v4무작위로 생성되지만 고유함을 보장하는 고한 ID=> 무작위 id를 레스트랑 이름으로 바꿈
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
@@ -52,5 +70,15 @@ app.get('/confirm', function (req, res) {
 app.get('/about', function (req, res) {
   res.render('about');
 });
+
+//유효하지 않은 페이지
+//다른 경로에서 처리되지 않은 요청이 있으면 404 에러 페이지 뜸
+app.use(function(req,res) {
+  res.render('404');
+});
+
+//좋은 사용자경험을 제공하기 위해 문제가 발생하면 최소한 사용자정의 메세지로 처리함
+//페이지 에러가 있을 때
+app.use(function(error, req, res, next){ res.render('500')});
 
 app.listen(3000);
