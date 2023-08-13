e144dc070c41706df089572caec23d356c8b585a
Jiin <yujiin4475@gmail.com>
2023-08-13 23:07:06 +0900

    자바스크립트 연습

--- function /코딩앙마.js
+++ function /코딩앙마.js
@@ -223,4 +223,324 @@ function hasCola(str){//콜라가 포함되어 있는지만 판별
     hasCola('콜라');//0
     
 
+//Array 
+//1. push() 뒤에 삽입
+//2. pop() 뒤에 삭제
+//3. unshift() 앞에 삽입
+//4. shift()  앞에 삭제
 
+//splice(n, m) 특정요소 지움 n번 째 시작해서 m 갯수 지우기
+let arr1 = [1,2,3,4,5];
+arr.splice(1, 2);
+console.log(arr1)// => [2,3,4]지워서 => [1,4,5]남음
+//splice(n, m, x) 특정요소 지움 n번 째 시작해서 m 갯수 지우기 그리고 x요소 추가
+arr1 = [1,2,3,4,5];
+arr.splice(1,3,100, 200);
+console.log(arr)// 1번부터 3번째 요소인 => 2,3,4 지우고 그 자리에 100, 200 추가해서 [ 1,100, 200, 5]
+
+arr = ['나는', '철수', '입니다'];
+
+arr.splice(1, 0, '대한민국', '소방관');
+console.log(arr)// m 이 0 이면 아무것도 지우지 않음 1번재 자리 _ 철수 그래서 0과 1사이에 추가 문자열 들어감
+//['나는', '대한민국', '소방관', '철수', '입니다']
+
+//splice() 삭제된 요소 반환
+arr = [1,2,3,4,5];
+let result1 = arr.splice(1,2)
+console.log(arr)// 삭제된 이후 배열에 남은 숫자[1,4,5]
+console.log(result1) // 삭제된 숫자를 반환[2,3]
+
+
+
+//slice(n,m) n 부터 m 까지 반환(m 번째는 반환하지 않고 그 전 숫자까지만 반환)
+arr = [1,2,3,4,5];
+arr.slice(1,4)// [2,3,4]
+
+arr = arr.slice();// 괄호안에 아무것도 없으면 그대로 전체 복사
+console.log(arr)//[1,2,3,4,5]
+
+
+
+//concat (arr2,arr3..) 합쳐서 새 배열 반환
+let arr3 = [1,2];
+
+arr3.concat([3,4]);//[1,2,3,4]
+arr3.concat([3,4],[5,6]);//[1,2,3,4,5,6]
+arr3.concat([3,4],5,6);//[1,2,3,4,5,6]
+
+//for.Each(fn) 배열의 반복 인수를 함수(fn)을 받음
+//forEach 는 매개변수 3개를 받는데 1. item은 해당 요소, 2. index 0,1,2 3. users 해당배열 자체
+
+let users = ['Mike', 'Tom',  'Jane'];
+users.forEach((name, index) => {
+console.log(name);
+});
+
+users = ['Mike', 'Tom',  'Jane'];
+users.forEach((name, index) => {
+console.log( `${index + 1}. ${name}`);
+});
+//=> 1. mlike 
+//=> 2. Tom
+//=> 3. Jane
+//이렇게 순회 가능
+
+//arr.infdexOF / arr.lastIndex Of
+
+arr = [1,2,3,4,5,1,2,3];
+arr.indexOf(3)// 2  없으면 -1 반환
+arr.indexOf(3,3)//7 => 
+arr.lastIndexOf(3);// 7 => 끝에서 부터 찾아서 
+
+//arr.includes()포함하는지 확인
+arr = [1,2,3];
+arr.includes(2);// true
+arr.includes(8);//false
+
+
+//arr.fine(fn), arr.fineIndex(fn) => includes와 비슷하지만 좀 더 정교하게 찾을 수 있게 인자를 함수로 받음
+//찾으면 true 반환, 없으면 undefined 반환 =>>>>>>> 만족하는 하나의 값만 찾음
+
+arr = [1,2,3,4,5,6];
+
+const result01 = arr.find((item) => { return item % 2 === 0;});
+
+console.log(result01);
+
+//fineIndex예제( 객체가 들어있는 배열)
+
+let userList  = [
+    {name: 'Mike', age: 30},
+    {name: 'Jane', age: 27},
+    {name: 'Tom', age: 10}
+];
+
+// const result02 = userList.find((user) => {
+//     if(user.age < 19) {
+//         return true;
+//     }
+//     return false;
+// });
+
+// console.log(result02); // {name: 'Tom', age: 10}
+
+// const result02 = userList.findIndex((user) => {
+//     if(user.age < 19) {
+//         return true;
+//     }
+//     return false;
+// });
+
+// console.log(result02); // 2
+
+
+
+//arr.filter(fn) => 만족하는 모든 요소를 배열로 반환
+arr = [1,2,3,4,5,6];
+const arr01 = arr.filter((item) => {
+  return item % 2 === 0 ;
+});
+
+console.log(arr01);
+
+
+//arr.reverse() 역순으로 재정렬
+const arr02 = [1,2,3,4,5];
+arr02.reverse();
+
+//arr.map(fn) 함수를 받아서 특정 기능을 시행하고 새로운 배열을 반환
+
+let userList01  = [
+    {name: 'Mike', age: 30},
+    {name: 'Jane', age: 27},
+    {name: 'Tom', age: 10}
+];
+
+
+//Object.assign({}, user);// 유저의 새로운 값을 부여 할  때 
+
+let newUserList = userList01.map((user, index) => {
+    return Object.assign({}, user, {
+        id: index + 1,
+        isAdult: user.age > 19,
+    });
+});
+
+console.log(userList01);//기존의 리스트
+console.log(newUserList);//새 리스트
+
+
+//배열을 합치기 Join, split
+
+let arr03 = ['안녕', '나는', '영희야!'];
+
+let result03 = arr03.join('  ');//()=> 아무것도 넣지 않으면 (,) 들어감 (' ') 공백넣기
+console.log(result03);
+
+let user3 = 'Mike, Jane,Tom,Tony';
+
+//split() 문자열을 배열로 바꿔줌 (',')=>쉼표 기준으로 나누기
+const result04 = user3.split(',');
+console.log(result04);// =>['Mike', 'Jane','Tom','Tony']
+
+
+let str01 = 'Hello, My name is Mike.!';
+const result05 = str01.split('');
+console.log(result05); //=>'H', 'e', 'l', 'l', 'o', ',', ' ', ]대충 이런식읨
+
+
+//Array.isArray() 배열인지 아닌지 확인하기
+ let user6 = {//객체
+    name: 'Mike',
+    age: 30,
+ };
+
+ let userList02 = ['Mike', 'Tom', 'Jane'];//배열
+
+
+ //구분하기 어려움
+ console.log(typeof user6);//object => 객체
+ console.log(typeof userList02);//object => 객체
+ 
+//구분하기
+console.log(Array.isArray(user6));//false => 객체
+ console.log(Array.isArray(userList02));//true => 객체
+
+ //arr.sort()/ arr.reduce() => 
+  //arr.sort() => 배열 재정렬, 배열 자체가 변경됨
+  let arr04 = [1,5,4,2,3];
+  arr04.sort();
+  console.log(arr04);//[1, 2, 3, 4, 5]
+
+//주의
+// let arr05 = [27,8,5,13];
+// arr05.sort();
+// console.log(arr05);//[13, 27, 5, 8] => 틀린 답임
+
+let arr05 = [27,8,5,13];
+
+// function fn (a,b){
+//     return a - b;
+// }
+// arr05.sort(fn);
+
+//============>
+arr05.sort((a,b)=> {return a - b ;})// a랑 b를 비교해서 A 가 크면 양수, 같으면 0, 작으면 음수 
+//=> a작으면 앞으로 보냄 0 은 가만히 있기, a가 크면 b 가 작은거니까 => b를 앞으로 보냄
+
+console.log(arr05);//[13, 27, 5, 8] => [5, 8, 13, 27]
+
+  let str02 = ['a','c', 'd','e', 'b'];
+  str02.sort();
+  console.log(str02);// ['a', 'b', 'c', 'd', 'e']
+
+
+//reduce()=> 배열의 모든 수 합치기
+//forEach 예시=========>
+let arr06 = [1,2,3,4,5];
+
+//for, for of , forEach
+
+let result06 = 0;
+arr06.forEach((num) => {
+    // result = result + num ;
+    result06+= num;
+});
+
+console.log(result06);
+
+//===> reduce() 활용 => 인수로 함수를 받음, 
+//(누적 계산값, 현재값) => {return 계산값};
+arr06 = [1,2,3,4,5];
+
+const result07 = arr.reduce((prev,cur) => {
+    return prev + cur ;
+}, 0);//0 => 초기값
+
+console.log(result07);
+
+let userList03 = [
+    {name:'Mike', age: 30},
+    {name:'Tom', age: 10},
+    {name:'Jane', age: 27},
+    {name:'Sue', age: 26},
+    {name:'harry', age: 42},
+    {name:'Steave', age: 60},
+
+];
+//19세 이상만 
+// let result08  = userList03.reduce((prev, cur) => {
+//     if(cur.age > 19) {
+//         prev.push(cur.name);
+//     }
+//     return prev;
+// }, []);
+// console.log(result08);
+
+
+//나이의 합
+// let result08  = userList03.reduce((prev, cur) => {
+//     return prev += cur.age ;
+// }, 0);
+// console.log(result08);
+
+//이름이 3letter 이상 = Tom
+
+let result08 = userList03.reduce((prev, cur)=> {
+if(cur.name.length === 3 ) {
+    prev.push(cur.name);
+}
+return prev;
+}, []);
+console.log(result08);
+
+
+//구조 분해 할당 => Destructuring assignment
+// 구조 분해 할당 구문은 배열이나 객체의 속성을 분해해서 그 값을 변수에 담을 수 있게 하는 표현식
+
+// let [x, y] = [1,2];
+// console.log(x);// => 1
+// console.log(y);// => 2
+
+let users01 = ['Mike','Tom','Jane'];
+let [user01 , user02, user03] = users01;
+
+console.log(user01);//mike
+console.log(user02);//tom
+console.log(user03);//jane
+
+//해당하는 값이 없을 때
+
+// let [e,f,g] = [1,2];//e=>1, f=>2, g=> undefined. 
+//to avoid an error  값을 미리 할당함
+let [e=3,f=4 ,g=5] = [1,2];
+console.log(e);//1
+console.log(f);//2
+console.log(g);//5 => 기본값
+
+//배열 구조 분해 : 일부 반환값 무시
+[user01, , user02] = ['Mike', 'Tom', 'Jane', 'Tony'];
+console.log(user01);//Mike 
+console.log(user02);//Jane
+
+//배열 구조 분해: 바꿔치기
+ a =1;
+ b =2;
+ [a,b] = [b,a]//바꿈
+
+ //객체 구조 분해
+//  user04 = {name: 'Mike', age:30};
+//  let {name, age} = user04;
+
+//  console.log(name);//=> Mike
+//  console.log(age);//=> 30
+
+//  //=> key값 바꿀 수 있음 : 새로운 변수 이름으로 할당
+//  let {name: userName,age:userAge} =user04;
+//  console.log(userName);//=> Mike
+//  console.log(userAge);//=> 30
+
+ //객체 구조 분해 : 기본값
+ let user04 = {name: 'Mike', age:30, gender: 'female'};
+ let {name, age, gender='male'} = user04; //gender=> undefined, gender= male
+ console.log(user04);
